2ba94d48645f88601ee6b7abd4cda9ed0d81c49c
Yash Mittal <yashmittalmm@gmail.com>
2024-12-20 18:20:22 +0530

    chore: Add tanstack react query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import QueryProvider from "@/providers/QueryProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
-  )
+  );
 }
